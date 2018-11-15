856358449b55a966c25fdf14f2ab682759e45b9c
Kamil Michna <kamilmichna>
2018-11-15 19:47:10 +0100

    Updates

---
+++ precache-manifest.b8845f780611b36726aacb8386555030.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f398bc34af7460b53f77",
+    "url": "/paint/static/js/runtime~main.f398bc34.js"
+  },
+  {
+    "revision": "9dfc168722ad12cd50b1",
+    "url": "/paint/static/js/main.9dfc1687.chunk.js"
+  },
+  {
+    "revision": "20bc491bae5e667480bb",
+    "url": "/paint/static/js/1.20bc491b.chunk.js"
+  },
+  {
+    "revision": "9dfc168722ad12cd50b1",
+    "url": "/paint/static/css/main.2f472278.chunk.css"
+  },
+  {
+    "revision": "b8ffb1e26c9f6a1a1239ea57b3afd70c",
+    "url": "/paint/index.html"
+  }
+];
